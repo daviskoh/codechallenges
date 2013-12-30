4b59ff84ce5319e1e53399b3ecef6134b9ce504c
daviskoh <koh.davis.0@gmail.com>
2013-12-30 12:38:12 -0500

    passing tests

--- rubeque/easy/separating numbers with commas/lib/comma.rb
+++ rubeque/easy/separating numbers with commas/lib/comma.rb
@@ -1,4 +1,18 @@
 # turn to string
 # reverse string
 # every 3 digits: add comma
+  # split every 3 characters
+  # join with comma
 # reverse and return
+
+def separate_with_comma(n)
+  result = n.to_s.reverse
+
+  i = 0
+  while i < result.length do 
+    result.insert(i, ',') if i % 4 == 0
+    i += 1
+  end
+
+  result.reverse[0..-2]
+end
