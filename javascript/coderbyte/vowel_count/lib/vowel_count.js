765cd946b66cbc183213f4947c4068eadfea5bec
daviskoh <koh.davis.0@gmail.com>
2014-02-28 18:34:50 -0500

    handles a simple case w/ 1 & w/ 2

--- javascript/coderbyte/vowel_count/lib/vowel_count.js
+++ javascript/coderbyte/vowel_count/lib/vowel_count.js
@@ -1,3 +1,14 @@
 exports.vowelCount = function(string) {
-  return string;
+  var vowels = ['a', 'e', 'i', 'o', 'u'];
+  var counter = 0;
+  // split string into characters
+  // if character is included in vowels array, plus one to counter
+  // return counter
+  string.split('').forEach(function(character) {
+    if (vowels.indexOf(character) !== -1) {
+      counter ++;
+    };
+  });
+
+  return counter;
 }
--- javascript/coderbyte/vowel_count/spec/vowel_count_spec.js
+++ javascript/coderbyte/vowel_count/spec/vowel_count_spec.js
@@ -4,4 +4,12 @@ describe('vowelCount', function() {
   it('does something', function() {
     expect(vowelCount('e')).not.toBe(null);
   });
+
+  it('handles a simple case with 1 vowel', function() {
+    expect(vowelCount('hell')).toBe(1);
+  });
+
+  it('handles a simple case with 2 vowels', function() {
+    expect(vowelCount('hello')).toBe(2);
+  });
 });
