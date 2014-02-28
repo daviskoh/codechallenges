efd667259d3b427476c66c3cac274c864712726b
daviskoh <koh.davis.0@gmail.com>
2014-02-28 18:28:08 -0500

    setup vowel count func & spec

---
+++ javascript/coderbyte/vowel_count/lib/vowel_count.js
@@ -0,0 +1,3 @@
+exports.vowelCount = function(string) {
+  return string;
+}
---
+++ javascript/coderbyte/vowel_count/spec/vowel_count_spec.js
@@ -0,0 +1,7 @@
+var vowelCount = require('../lib/vowel_count').vowelCount;
+
+describe('vowelCount', function() {
+  it('does something', function() {
+    expect(vowelCount('e')).not.toBe(null);
+  });
+});
