cc57834b2adf67f06dfae880b4e1824fa0ec0d0d
daviskoh <koh.davis.0@gmail.com>
2014-03-02 17:15:48 -0500

    reverses a string of many word keeping word order

--- javascript/reverse_string_mocha/lib/reverse_string.js
+++ javascript/reverse_string_mocha/lib/reverse_string.js
@@ -1,3 +1,5 @@
 module.exports = function(string) {
+  console.log(string.split(''));
+  console.log(string.split('').reverse());
   return string.split('').reverse().join('');
 }
--- javascript/reverse_string_mocha/test/reverse_string_test.js
+++ javascript/reverse_string_mocha/test/reverse_string_test.js
@@ -5,4 +5,8 @@ describe('reverseString', function() {
   it('reverses a 1 word string', function() {
     reverseString('ello').should.eql('olle');
   });
+
+  it('reverses a string of many words keeping word order', function() {
+    reverseString('ello there').should.eql('ereht olle');
+  });
 });
