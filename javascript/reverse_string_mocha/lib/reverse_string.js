3d593ebc895be41e911be9dbb2798961105460de
daviskoh <koh.davis.0@gmail.com>
2014-03-02 19:32:01 -0500

    encapsulate; create helper method

--- javascript/reverse_string_mocha/lib/reverse_string.js
+++ javascript/reverse_string_mocha/lib/reverse_string.js
@@ -1,5 +1,23 @@
+function singleWordReverser (word) {
+  var punctuation = [',', '.', '!', '?', "'"];
+  
+  for (var i = 0; i < word.length; i++) {
+    // return substring + punctuation
+    if (punctuation.indexOf(word[i]) !== -1) {
+      var puncIndex = i;
+      return word.substring(0, puncIndex) + word.substring(puncIndex, word.length - 1);
+    } else {
+      return  word.split('').reverse().join('');
+    };
+  };
+}
+
 module.exports = function(string) {
-  console.log(string.split(''));
-  console.log(string.split('').reverse());
-  return string.split('').reverse().join('');
+  // return string.split('').reverse().join('');
+  // split by white space
+  return string.split(/\s+/).map(function(word) {
+    // handle each word group separately
+      // extract position of punctuation
+      return singleWordReverser(word);
+  }).join(' ');
 }
--- javascript/reverse_string_mocha/test/reverse_string_test.js
+++ javascript/reverse_string_mocha/test/reverse_string_test.js
@@ -7,6 +7,10 @@ describe('reverseString', function() {
   });
 
   it('reverses a string of many words keeping word order', function() {
-    reverseString('ello there').should.eql('ereht olle');
+    reverseString('ello there').should.eql('olle ereht');
+  });
+
+  it('reverses a string keeping origin punctuation', function() {
+    reverseString('ello, there').should.eql('olle, ereht');
   });
 });
