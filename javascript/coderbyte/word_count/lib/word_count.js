88588401fec2eeb4c69f2a88e13a77f18a128e81
daviskoh <koh.davis.0@gmail.com>
2014-03-04 15:31:09 -0500

    handles a simpel case without punctuation

--- javascript/coderbyte/word_count/lib/word_count.js
+++ javascript/coderbyte/word_count/lib/word_count.js
@@ -1,3 +1,6 @@
 exports.wordCount = function(string) {
-  
+  // split string via whitespace
+  // return length of array
+
+  return string.split(/\s/g).length;
 }
--- javascript/coderbyte/word_count/spec/word_count_spec.js
+++ javascript/coderbyte/word_count/spec/word_count_spec.js
@@ -4,4 +4,8 @@ describe('wordCount', function() {
   it('is a function', function() {
     expect(typeof wordCount === 'function').toBe(true);
   });
+
+  it('handles a simple case without punctuation', function() {
+    expect(wordCount('Hello World')).toBe(2);
+  });
 });
