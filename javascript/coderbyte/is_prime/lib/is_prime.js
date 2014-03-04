64d52b996c5710f71edb48668b69402bd8d28db6
daviskoh <koh.davis.0@gmail.com>
2014-03-04 15:40:51 -0500

    returns false for 0 and 1

--- javascript/coderbyte/is_prime/lib/is_prime.js
+++ javascript/coderbyte/is_prime/lib/is_prime.js
@@ -1,3 +1,3 @@
 exports.isPrime = function(n) {
-  
+  if (n === 0 || n === 1) { return 'false' };
 }
--- javascript/coderbyte/is_prime/spec/is_prime_spec.js
+++ javascript/coderbyte/is_prime/spec/is_prime_spec.js
@@ -4,4 +4,12 @@ describe('isPrime', function() {
   it('is a function', function() {
     expect(typeof isPrime === 'function').toBe(true);
   });
+
+  it('returns false for 0', function() {
+    expect(isPrime(0)).toBe('false');
+  });
+
+  it('returns false for 1', function() {
+    expect(isPrime(1)).toBe('false');
+  });
 });
