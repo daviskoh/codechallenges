53571466512953be9d5f07b53aaaea2a2a810df4
daviskoh <koh.davis.0@gmail.com>
2014-03-07 17:51:02 -0500

    setup spec

---
+++ ruby/rubymonk/non_duplicated_values/spec/non_duplicated_values_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+require_relative '../lib/non_duplicated_values'
+
+describe '#non_duplicated_values' do 
+  it 'does something' do 
+    expect(non_duplicated_values).to_not eq(nil)
+  end
+end
