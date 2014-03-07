5a63e6814af01e8cac9fc996366dfb81069be838
daviskoh <koh.davis.0@gmail.com>
2014-03-07 17:20:52 -0500

    method returns something

--- javascript/rubymonk/length_of_strings/spec/length_of_strings_spec.rb
+++ javascript/rubymonk/length_of_strings/spec/length_of_strings_spec.rb
@@ -1,3 +1,8 @@
 require 'spec_helper'
 require_relative '../lib/length_of_strings'
 
+describe '#length_of_strings' do 
+  it 'does something' do 
+    expect(length_of_strings).to_not eq(nil)
+  end
+end
