d613d338cc6c693f517e52223a837201bca3610e
daviskoh <koh.davis.0@gmail.com>
2014-03-07 17:23:50 -0500

    method now takes params

--- javascript/rubymonk/length_of_strings/lib/length_of_strings.rb
+++ javascript/rubymonk/length_of_strings/lib/length_of_strings.rb
@@ -1,6 +1,6 @@
 # Given an array containing some strings,
 # return an array containing the length of those strings
 
-def length_of_strings
+def length_of_strings(array)
   ''
 end
--- javascript/rubymonk/length_of_strings/spec/length_of_strings_spec.rb
+++ javascript/rubymonk/length_of_strings/spec/length_of_strings_spec.rb
@@ -3,6 +3,10 @@ require_relative '../lib/length_of_strings'
 
 describe '#length_of_strings' do 
   it 'does something' do 
-    expect(length_of_strings).to_not eq(nil)
+    expect(length_of_strings(['ruby'])).to_not eq(nil)
+  end
+
+  it 'correct value for array of 1 string' do 
+    expect(length_of_strings ['ruby']).to eq([4])
   end
 end
