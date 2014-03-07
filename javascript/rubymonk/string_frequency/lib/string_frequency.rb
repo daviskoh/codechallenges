0e930e371608fce1016271e5c6eab04c4d3b177b
daviskoh <koh.davis.0@gmail.com>
2014-03-07 17:31:46 -0500

    method now takes 2 params

--- javascript/rubymonk/string_frequency/lib/string_frequency.rb
+++ javascript/rubymonk/string_frequency/lib/string_frequency.rb
@@ -1,6 +1,6 @@
 # Given a sentence containing multiple words,
 # find the frequency of a given word in that sentence.
 
-def string_frequency
+def string_frequency(sentence, word)
   ''
 end
--- javascript/rubymonk/string_frequency/spec/string_frequency_spec.rb
+++ javascript/rubymonk/string_frequency/spec/string_frequency_spec.rb
@@ -3,6 +3,10 @@ require_relative '../lib/string_frequency'
 
 describe '#string_frequency' do 
   it 'does something' do 
-    expect(string_frequency).to_not eq(nil)
+    expect(string_frequency('ello fred', 'ello')).to_not eq(nil)
+  end
+
+  it 'should handle a simple sentence & word' do 
+    expect(string_frequency('ello fred', 'ello')).to eq(1)
   end
 end
