3e14e5259254c3fdc7faeb59a222fbfe59fdf05b
daviskoh <koh.davis.0@gmail.com>
2014-03-07 17:28:53 -0500

    setup dirs & tests

---
+++ javascript/rubymonk/string_frequency/spec/string_frequency_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+require_relative '../lib/string_frequency'
+
+describe '#string_frequency' do 
+  it 'does something' do 
+    expect(string_frequency).to_not eq(nil)
+  end
+end
