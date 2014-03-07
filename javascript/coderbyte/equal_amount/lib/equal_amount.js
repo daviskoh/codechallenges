f12ff18865d3cf54933983cdf43b7bb7ccbf569a
daviskoh <koh.davis.0@gmail.com>
2014-03-07 17:08:28 -0500

    all tests pass

--- javascript/coderbyte/equal_amount/lib/equal_amount.js
+++ javascript/coderbyte/equal_amount/lib/equal_amount.js
@@ -1,3 +1,19 @@
 exports.equalAmount = function(string) {
+  // check whether string of x's & o's
+  var xCount = 0;
+  var oCount = 0;
+  // if x count === o count
+  string.split('').forEach(function(character) {
+    if (character === 'x') {
+      xCount++;
+    } else {
+      oCount++;
+    };
+  });
   
+  if (xCount === oCount) {
+    return 'true';
+  } else {
+    return 'false';
+  };
 }
--- javascript/coderbyte/equal_amount/spec/equal_amount_spec.js
+++ javascript/coderbyte/equal_amount/spec/equal_amount_spec.js
@@ -5,5 +5,19 @@ describe('equalAmount', function() {
     expect(typeof equalAmount).toBe('function');
   });
 
-  
+  it('should handle a simple true case', function() {
+    expect(equalAmount('xo')).toBe('true');
+  });
+
+  it('should handle a simple false case', function() {
+    expect(equalAmount('x')).toBe('false');
+  });
+
+  it('should handle a complex true case', function() {
+    expect(equalAmount('xoxoxo')).toBe('true');
+  });
+
+  it('should handle a complex false case', function() {
+    expect(equalAmount('xoooox')).toBe('false');
+  });
 });
