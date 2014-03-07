ff18581466a973cb139853b779685247a8f9a575
daviskoh <koh.davis.0@gmail.com>
2014-03-07 17:00:22 -0500

    prep equal amount challenge

---
+++ javascript/coderbyte/equal_amount/spec/equal_amount_spec.js
@@ -0,0 +1,9 @@
+var equalAmount = require('../lib/equal_amount').equalAmount;
+
+describe('equalAmount', function() {
+  it('is a function', function() {
+    expect(typeof equalAmount).toBe('function');
+  });
+
+  
+});
