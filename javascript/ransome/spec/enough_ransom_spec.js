cc2b63dcc66b36269ba79a9edae85c07a847b9d7
daviskoh <koh.davis.0@gmail.com>
2014-03-12 17:49:11 -0400

    init tests

---
+++ javascript/ransome/spec/enough_ransom_spec.js
@@ -0,0 +1,7 @@
+var enoughRansom = require('../lib/enough_ransom');
+
+describe('enoughRansom', function() {
+  it('is a function', function() {
+    expect(typeof enoughRansom).toBe('function');
+  });
+});
