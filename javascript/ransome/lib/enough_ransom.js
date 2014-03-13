19e1ccb8e2233c232c4b1692254c77d4b53df88f
daviskoh <koh.davis.0@gmail.com>
2014-03-12 20:32:38 -0400

    organize notes

--- javascript/ransome/lib/enough_ransom.js
+++ javascript/ransome/lib/enough_ransom.js
@@ -4,16 +4,13 @@
 
 module.exports = function(ransomNote, magazine) {
   // split ransomNote
-  // for each character
-    // check if magazine contains it
-      // if any character not contained return false
-
-  // return true
-
   var ransomArray = ransomNote.toLowerCase().replace(/\s+/g, '').split('');
   var magazineLower = magazine.toLowerCase();
 
+  // for each character
   for (var i = ransomArray.length - 1; i >= 0; i--) {
+    // check if magazine contains it
+    // if any character not contained return false
     if (magazineLower.indexOf(ransomArray[i]) === -1) {
       return false;
     };
