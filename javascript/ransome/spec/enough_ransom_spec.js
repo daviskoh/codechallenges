fc3ac45da69937db925e5684cf28795b6973aaa6
daviskoh <koh.davis.0@gmail.com>
2014-03-12 20:12:55 -0400

    define vars at top

--- javascript/ransome/spec/enough_ransom_spec.js
+++ javascript/ransome/spec/enough_ransom_spec.js
@@ -1,17 +1,20 @@
 var enoughRansom = require('../lib/enough_ransom');
 
 describe('enoughRansom', function() {
+  var ransomNote, magazine;
+
   it('is a function', function() {
     expect(typeof enoughRansom).toBe('function');
   });
 
   it('handles a simple case', function() {
-    var ransomNote = 'a';
-    var magazine = 'ab';
+    ransomNote = 'a';
+    magazine = 'ab';
     expect(enoughRansom(ransomNote, magazine)).toBe(true);
   });
 
   it('is case insensitive', function() {
-    
+    ransomNote = 'a';
+    magazine = 'ab';
   });
 });
