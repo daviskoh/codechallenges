1079ea826cff8512453ca2086925f7f3d348a219
daviskoh <koh.davis.0@gmail.com>
2014-03-14 18:06:20 -0400

    remove restrictions

--- javascript/recursive/length_of_strings/lib/length_of_strings.js
+++ javascript/recursive/length_of_strings/lib/length_of_strings.js
@@ -1,7 +1,5 @@
 // Given an array containing some strings,
 // return an array containing the length of those strings.
-// cant use: .length or for loop
-
 module.exports = function(array) {
-
+  
 }
