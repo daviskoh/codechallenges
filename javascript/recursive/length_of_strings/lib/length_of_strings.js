62500da24849ef7e091ae6d2414bdba73d488214
daviskoh <koh.davis.0@gmail.com>
2014-03-14 18:03:35 -0400

    add restrictions

--- javascript/recursive/length_of_strings/lib/length_of_strings.js
+++ javascript/recursive/length_of_strings/lib/length_of_strings.js
@@ -1,6 +1,7 @@
 // Given an array containing some strings,
 // return an array containing the length of those strings.
+// cant use: .length or for loop
 
-module.exports = function() {
+module.exports = function(array) {
 
 }
--- javascript/recursive/length_of_strings/spec/length_of_strings_spec.js
+++ javascript/recursive/length_of_strings/spec/length_of_strings_spec.js
@@ -4,4 +4,8 @@ describe('lengthOfStrings', function() {
   it('is a function', function() {
     expect(typeof lengthOfStrings).toBe('function');
   });
+
+  it('handles simple case', function() {
+    expect(lengthOfStrings(['array'])).toBe([1]);
+  });
 });
