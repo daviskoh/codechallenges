073a8c647b690dc14306bf0bd62de8f4a6d878aa
daviskoh <koh.davis.0@gmail.com>
2014-03-14 17:59:35 -0400

    1st spec

---
+++ javascript/recursive/length_of_strings/spec/length_of_strings_spec.js
@@ -0,0 +1,7 @@
+var lengthOfStrings = require('../lib/length_of_strings');
+
+describe('lengthOfStrings', function() {
+  it('is a function', function() {
+    expect(typeof lengthOfStrings).toBe('function');
+  });
+});
