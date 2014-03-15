3f85eb46e0de2cb8a91471d2ec5f0b7e93c91321
daviskoh <koh.davis.0@gmail.com>
2014-03-14 20:03:28 -0400

    handles recursively

--- javascript/recursive/average/lib/average.js
+++ javascript/recursive/average/lib/average.js
@@ -1,5 +1,27 @@
 // write a function that calculates the average of its arguments
+function average(numbers) {
+  var numerator = 0;
+  var denominator;
 
-module.exports = function() {
+  var calculateAverage = function(numbersArray) {
+    // base case
+    // if arguments list is empty,
+    if (numbers.length === 0) {
+      return numerator / denominator;
+    };
 
+    // recursive case
+      // take next number & add to numerator
+      // increment denominator by 1
+    var currrentNumber = numbers.shift();
+    numerator += currrentNumber;
+    denominator = currrentNumber;
+
+    return calculateAverage(numbers);
+  }
+
+  return calculateAverage(numbers);
 }
+
+
+module.exports = average;
--- javascript/recursive/average/spec/average_spec.js
+++ javascript/recursive/average/spec/average_spec.js
@@ -4,4 +4,8 @@ describe('average', function() {
   it('is a function', function() {
     expect(typeof average).toBe('function');
   });
+
+  it('handles simple case', function() {
+    expect(average([1,2,3])).toBe(2);
+  });
 });
