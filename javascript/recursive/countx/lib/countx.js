bee09d5b36954e45c6c23ab3e5b46004943457b4
daviskoh <koh.davis.0@gmail.com>
2014-03-16 16:44:50 -0400

    add description

--- javascript/recursive/countx/lib/countx.js
+++ javascript/recursive/countx/lib/countx.js
@@ -1,5 +1,10 @@
-function countx() {
-  
+// Given a string, compute recursively (no loops): 
+// the number of lowercase 'x' chars in the string. 
+
+function countx(string) {
+  // base case
+
+  // recursive case
 }
 
 module.exports = countx;
--- javascript/recursive/countx/spec/countx_spec.js
+++ javascript/recursive/countx/spec/countx_spec.js
@@ -4,4 +4,8 @@ describe('countx', function() {
   it('is a function', function() {
     expect(typeof countx).toBe('function');
   });
+
+  it('recognizes x', function() {
+    expect(countx('x')).toBe(1);
+  });
 });
