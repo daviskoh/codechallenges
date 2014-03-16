6d5b950a9cdfc7e02cee06bb287baab761cb0d2e
daviskoh <koh.davis.0@gmail.com>
2014-03-16 16:47:57 -0400

    recognizes x

--- javascript/recursive/countx/lib/countx.js
+++ javascript/recursive/countx/lib/countx.js
@@ -3,8 +3,16 @@
 
 function countx(string) {
   // base case
+  if (string.length === 0) {
+    return 0;
+  };
 
   // recursive case
+  if (string.charAt(string.length - 1) === 'x') {
+    return 1 + countx(string.substring(1));
+  };
+
+  return countx(string.substring(1));
 }
 
 module.exports = countx;
