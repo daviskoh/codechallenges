367bf0900b25ecef3cea420edcf15d8447c4c3c7
daviskoh <koh.davis.0@gmail.com>
2014-03-18 14:27:49 -0400

    initial

---
+++ javascript/recursive/change_pi/lib/change_pi.js
@@ -0,0 +1,5 @@
+function changePi() {
+  
+}
+
+module.exports = changePi;
---
+++ javascript/recursive/change_pi/spec/change_pi_spec.js
@@ -0,0 +1,7 @@
+var changePi = require('../lib/change_pi');
+
+describe('changePi', function() {
+  it('is a function', function() {
+    expect(typeof changePi).toBe('function');
+  });
+});
