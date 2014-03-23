8981348263df917f37ccd871dcd0b8fc468fa92b
daviskoh <koh.davis.0@gmail.com>
2014-03-23 18:00:25 -0400

    handles base case

--- javascript/recursive/array11/lib/array11.js
+++ javascript/recursive/array11/lib/array11.js
@@ -1,5 +1,15 @@
-var array11 = function() {
+// Given an array of ints,
+// compute the number of times that the value 11 appears in the array.
+// We'll use the convention of considering only the part of the array
+// that begins at the given index.
+// In this way, a recursive call can pass index+1 to move down the array.
+// The initial call will pass in index as 0. 
 
+var array11 = function(array, initial) {
+  // base case
+  if (array.length === 0) {
+    return 0;
+  };
 };
 
 module.exports = array11;
--- javascript/recursive/array11/spec/array11_spec.js
+++ javascript/recursive/array11/spec/array11_spec.js
@@ -4,4 +4,8 @@ describe('array11', function() {
   it('is a function', function() {
     expect(typeof array11).toBe('function');
   });
+
+  it('handles a base case', function() {
+    expect(array11([], 0)).toBe(0);
+  });
 });
