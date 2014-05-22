98bfbdae6b1a8a41635c683e5f1fac68e2d71a55
daviskoh <koh.davis.0@gmail.com>
2014-05-21 23:43:05 -0400

    edit

--- javascript/sum_arrays/spec/sum_arrays_spec.js
+++ javascript/sum_arrays/spec/sum_arrays_spec.js
@@ -1,16 +1,16 @@
-sum_arrays = require('../lib/sum_arrays')
+var sum_arrays = require('../lib/sum_arrays')
 
 describe('sum_arrays', function() {
   it('returns content is array contains only 0', function() {
     expect(sum_arrays([0])).toEqual(0)
   })
 
-  it('returns the only number in array if array contains 1 number', function() {
-    expect(sum_arrays([1, 'ello'])).toEqual(1)
-  })
-
   it('takes a adds numbers in a 1 level deep array', function() {
     expect(sum_arrays([1,2,3,4,5])).toEqual(15)
+  })
+
+  it('returns the only number in array if array contains 1 number', function() {
+    expect(sum_arrays([1, 'ello'])).toEqual(1)
   })
 
   it('takes a adds ONLY numbers in a 1 level deep array', function() {
