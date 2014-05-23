749e8670f81be4604ae99a2afa451a0c31bd7050
daviskoh <koh.davis.0@gmail.com>
2014-05-23 13:46:14 -0400

    non recursive func

--- javascript/recursive/count_abc/spec/count_abc_spec.js
+++ javascript/recursive/count_abc/spec/count_abc_spec.js
@@ -1,31 +1,38 @@
-var countABC = require('../lib/count_abc.js');
+var countABCRecursive = require('../lib/count_abc.js').countABCRecursive;
+var countABCRegular = require('../lib/count_abc.js').countABCRegular;
 
-describe('countABC', function() {
+describe('countABCRecursive', function() {
     it('is a function', function () {
-        expect(typeof countABC).toBe('function');
+        expect(typeof countABCRecursive).toBe('function');
     });
 
     it('handles a case with no instances of abc or aba', function () {
-        expect(countABC('meow')).toBe(0);
+        expect(countABCRecursive('meow')).toBe(0);
+        expect(countABCRegular('meow')).toBe(0);
     });
 
     it('handles a single instance of abc', function () {
-        expect(countABC('abc')).toBe(1);
+        expect(countABCRecursive('abc')).toBe(1);
+        expect(countABCRegular('abc')).toBe(1);
     });
 
     it('handles a single instance of aba', function () {
-        expect(countABC('aba')).toBe(1);
+        expect(countABCRecursive('aba')).toBe(1);
+        expect(countABCRegular('aba')).toBe(1);
     });
 
     it('handles multiple cases of abc', function () {
-        expect(countABC('abcxxabc')).toBe(2);
+        expect(countABCRecursive('abcxxabc')).toBe(2);
+        expect(countABCRegular('abcxxabc')).toBe(2);
     });
 
     it('handles multiple cases of aba', function () {
-        expect(countABC('abaxxabaxxaba')).toBe(3);
+        expect(countABCRecursive('abaxxabaxxaba')).toBe(3);
+        expect(countABCRegular('abaxxabaxxaba')).toBe(3);
     });
 
     it('handles multiple cases of both abc & aba', function () {
-        expect(countABC('abaxxxabcxxxabaxxxabaxxxabc')).toBe(5);
+        expect(countABCRecursive('abaxxxabcxxxabaxxxabaxxxabc')).toBe(5);
+        expect(countABCRegular('abaxxxabcxxxabaxxxabaxxxabc')).toBe(5);
     });
 });
