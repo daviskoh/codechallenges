f0127561ffe459e5d92ecad375d74397d1db432b
daviskoh <koh.davis.0@gmail.com>
2014-06-01 19:53:44 -0400

    handles empty string & lenght 1

--- javascript/length_of_strings/lib/length_of_strings.js
+++ javascript/length_of_strings/lib/length_of_strings.js
@@ -1,5 +1,9 @@
 function lengthOfStrings (string) {
-    
+    if (string.length === 0) {
+        return 0;
+    }
+
+    return 1;
 }
 
 module.exports = lengthOfStrings;
--- javascript/length_of_strings/spec/length_of_strings_spec.js
+++ javascript/length_of_strings/spec/length_of_strings_spec.js
@@ -4,4 +4,12 @@ describe('lengthOfStrings', function() {
     it('is a function', function () {
         expect(typeof lengthOfStrings).toBe('function');
     });
+
+    it('handles an empty string', function () {
+        expect(lengthOfStrings('')).toBe(0);
+    });
+
+    it('handles a string of length 1', function () {
+        expect(lengthOfStrings('a')).toBe(1);
+    });
 });
