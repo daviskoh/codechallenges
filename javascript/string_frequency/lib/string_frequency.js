192784cbacbb74123182dfc4016659a56448d70f
daviskoh <koh.davis.0@gmail.com>
2014-06-03 20:14:26 -0400

    init

---
+++ javascript/string_frequency/lib/string_frequency.js
@@ -0,0 +1,5 @@
+function stringFrequency (string, substring) {
+    
+}
+
+module.exports = stringFrequency;
---
+++ javascript/string_frequency/spec/string_frequency_spec.js
@@ -0,0 +1,7 @@
+var stringFrequency = require('../lib/string_frequency.js');
+
+describe('stringFrequency', function() {
+    it('is a function', function () {
+        expect(typeof stringFrequency).toBe('function');
+    });
+});
