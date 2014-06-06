a9093698df0e65f3b7ff2a0ce11b2e2c7a972894
daviskoh <koh.davis.0@gmail.com>
2014-06-05 23:31:15 -0400

    init commit

---
+++ javascript/recursive/count_hi_2/lib/count_hi_2.js
@@ -0,0 +1,5 @@
+function countHi2 (argument) {
+    
+}
+
+module.exports = countHi2;
---
+++ javascript/recursive/count_hi_2/spec/count_hi_2_spec.js
@@ -0,0 +1,7 @@
+var countHi2 = require('../lib/count_hi_2.js');
+
+describe('countHi2', function() {
+    it('is a function', function () {
+        expect(typeof countHi2).toBe('function');
+    });
+});
