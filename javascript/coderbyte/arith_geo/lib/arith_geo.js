4246138a9bda24c96d6091205aada171e0dd1aed
daviskoh <koh.davis.0@gmail.com>
2014-11-26 12:55:11 -0500

    refactored: O(n + 1) -> O(n)

--- javascript/coderbyte/arith_geo/lib/arith_geo.js
+++ javascript/coderbyte/arith_geo/lib/arith_geo.js
@@ -1,31 +1,21 @@
 // Arithmetic: diff bet each numbers is consistent
 // Geometric: each term after 1st is multiplied by previous
 
-function isArith(array) {
-    var arithDiff = array[1] - array[0];
-
-    for (var i = 1; i < array.length; i++) {
-        if (array[i] - array[i - 1] !== arithDiff) return false;
-    }
-
-    return true;
-}
-
-function isGeo(array) {
-    var geoDiff = array[1] / array[0];
-
-    for (var i = 1; i < array.length; i++) {
-        if (array[i] / array[i-1] !== geoDiff) return false;
-    }
-
-    return true;
-}
-
 function arithGeo (array) {
     if (array.length < 2) return -1;
 
-    if (isArith(array)) return 'Arithmetic';
-    if (isGeo(array)) return 'Geometric';
+    var arithDiff = array[1] - array[0],
+        geoDiff = array[1] / array[0]
+        arithResult = true,
+        geoResult = true;
+
+    for (var i = 1; i < array.length; i++) {
+        if (array[i] - array[i -1] !== arithDiff) arithResult = false;
+        if (array[i] / array[i -1] !== geoDiff) geoResult = false;
+    }
+
+    if (arithResult) return 'Arithmetic';
+    if (geoResult) return 'Geometric';
 
     return -1;
 }
