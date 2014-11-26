6ed8985ce4e26d0d83752118bab8cb77de68e4d1
daviskoh <koh.davis.0@gmail.com>
2014-11-26 11:58:28 -0500

    initial dir setup

---
+++ javascript/coderbyte/arith_geo/lib/arith_geo.js
@@ -0,0 +1,5 @@
+function arithGeo (argument) {
+    
+}
+
+module.exports = arithGeo;
---
+++ javascript/coderbyte/arith_geo/spec/arith_geo_spec.js
@@ -0,0 +1,7 @@
+var arithGeo = require('../lib/arith_geo.js');
+
+describe('arithGeo', function() {
+    it('is a function', function () {
+        expect(typeof arithGeo).toBe('function');
+    });
+});
