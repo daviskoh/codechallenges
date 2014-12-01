cf8ed9f0d2edd4413bf65135b6b804f22b9feb03
daviskoh <koh.davis.0@gmail.com>
2014-12-01 14:12:24 -0500

    1st solution

--- javascript/coderbyte/letter_count_i/lib/letter_count_i.js
+++ javascript/coderbyte/letter_count_i/lib/letter_count_i.js
@@ -1,5 +1,50 @@
-function letterCountI (argument) {
-    
+// split string into array
+// loop through array
+// map into array of repeated letter counts
+// sort array then return last
+
+function uniqueChars(string) {
+    var chars = [];
+
+    for (var i = 0; i < string.length; i++) {
+        if (chars.indexOf(string[i] !== -1)) chars.push(string[i]);
+    }
+
+    return chars;
+}
+
+function singleLetterCount(string, letter) {
+    if (!string.length) return 0;
+
+    if (string[0].indexOf(letter) !== -1) return 1 + singleLetterCount(string.substring(1), letter);
+
+    return singleLetterCount(string.substring(1), letter);
+}
+
+function letterCountI (string) {
+    if (!string.length) return -1;
+
+    var stringArray = string.split(/[ ]+/),
+        letterCountArray = [],
+        resultCount = 1;
+        result = '';
+
+    for (var i = 0; i < stringArray.length; i++) {
+        var uniqueCharsArray = uniqueChars(stringArray[i]);
+        console.log(uniqueCharsArray);
+
+        for (var j = 0; j < uniqueCharsArray.length; j++) {
+            var currentCount = singleLetterCount(stringArray[i], uniqueCharsArray[j]);
+            
+            // if currentCount > resultCount then result = stringArray[i]
+            if (currentCount > resultCount) {
+                resultCount = currentCount;
+                result = stringArray[i];
+            }
+        }
+    }
+
+    return result || -1;
 }
 
 module.exports = letterCountI;
--- javascript/coderbyte/letter_count_i/spec/letter_count_i.spec.js
+++ javascript/coderbyte/letter_count_i/spec/letter_count_i.spec.js
@@ -4,4 +4,20 @@ describe('letterCountI', function() {
     it('is a function', function () {
         expect(typeof letterCountI).toBe('function');
     });
+
+    it('should handle an empty string', function () {
+        expect(letterCountI('')).toBe(-1);
+    });
+
+    it('should handle a single word string', function () {
+        expect(letterCountI('meow')).toBe(-1);
+    });
+
+    it('should handle a simple case', function () {
+        expect(letterCountI('hello due')).toBe('hello');
+    });
+
+    it('should handle a complex case', function () {
+        expect(letterCountI('red none yellow')).toBe('none');
+    });
 });
