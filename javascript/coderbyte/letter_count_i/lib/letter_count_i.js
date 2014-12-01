435b57cfd58fbad6aa92620d5bae1d25a663d701
daviskoh <koh.davis.0@gmail.com>
2014-12-01 13:42:41 -0500

    init letter count i

---
+++ javascript/coderbyte/letter_count_i/lib/letter_count_i.js
@@ -0,0 +1,5 @@
+function letterCountI (argument) {
+    
+}
+
+module.exports = letterCountI;
---
+++ javascript/coderbyte/letter_count_i/spec/letter_count_i.spec.js
@@ -0,0 +1,7 @@
+var letterCountI = require('../lib/letter_count_i.js');
+
+describe('letterCountI', function() {
+    it('is a function', function () {
+        expect(typeof letterCountI).toBe('function');
+    });
+});
