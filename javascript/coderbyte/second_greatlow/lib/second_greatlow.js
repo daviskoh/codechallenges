6053b850f07d44109414adaa9b73b94c572aa3c0
daviskoh <koh.davis.0@gmail.com>
2014-12-01 14:30:17 -0500

    handle more complex case of numbers

--- javascript/coderbyte/second_greatlow/lib/second_greatlow.js
+++ javascript/coderbyte/second_greatlow/lib/second_greatlow.js
@@ -1,5 +1,16 @@
+// take the array of numbers stored in arr and return the second lowest and second greatest numbers,
+// respectively, separated by a space.
+// For example: if arr contains [7, 7, 12, 98, 106] the output should be 12 98.
+// The array will not be empty and will contain at least 2 numbers.
+
+function sortNumber(a, b) {
+    return a - b;
+}
+
 function secondGreatlow (arr) {
-    
+    var sortedArray = arr.sort(sortNumber);
+
+    return arr[1] + ' ' + arr[arr.length - 2];
 }
 
 module.exports = secondGreatlow;
--- javascript/coderbyte/second_greatlow/spec/second_greatlow.spec.js
+++ javascript/coderbyte/second_greatlow/spec/second_greatlow.spec.js
@@ -4,4 +4,12 @@ describe('secondGreatlow', function() {
     it('is a function', function () {
         expect(typeof secondGreatlow).toBe('function');
     });
+
+    it('should handle a simple case', function () {
+        expect(secondGreatlow([7, 7, 7])).toBe('7 7');
+    });
+
+    it('should handle different numbers', function () {
+        expect(secondGreatlow([1,5,8,2])).toBe('2 5');
+    });
 });
