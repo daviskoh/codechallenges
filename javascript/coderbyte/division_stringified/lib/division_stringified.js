3c7ed8d1d81a49bc8965fe89e8aa680e63824573
daviskoh <koh.davis.0@gmail.com>
2014-12-02 22:13:59 -0500

    fin division stringified

--- javascript/coderbyte/division_stringified/lib/division_stringified.js
+++ javascript/coderbyte/division_stringified/lib/division_stringified.js
@@ -2,8 +2,29 @@
 // return result as string w/ properly formatted commas
 // if result is 3 digits long, return num w/ no commas
 
+// 1,000 -------- 000,1
+// 100,000 ------ 000,001
+// 1,000,000 ---- 000,000,1
+
 function divisionStringified (n1, n2) {
-    return (n1 / n2).toString();
+    var numString = Math.round(n1 / n2).toString();
+    
+    if (numString.length < 4) return numString;
+
+    // reverse numString
+    // split numString into groups of 3 chars
+    // iterate through array
+        // if element length === 3 && next element exists
+        // then add commas to each end 
+
+    var reverseNumString = numString.split('').reverse().join('').match(/.{1,3}/g);
+    for (var i = 0; i < reverseNumString.length; i++) {
+        if (reverseNumString[i].length === 3 && reverseNumString[i + 1]) {
+            reverseNumString[i] += ',';
+        }
+    }
+
+    return reverseNumString.join('').split('').reverse().join('');
 }
 
 module.exports = divisionStringified;
--- javascript/coderbyte/division_stringified/spec/division_stringified.spec.js
+++ javascript/coderbyte/division_stringified/spec/division_stringified.spec.js
@@ -8,4 +8,21 @@ describe('divisionStringified', function() {
     it('should handle a number less than 4 digits', function () {
         expect(divisionStringified(3, 1)).toBe('3');
     });
+
+    it('should handle a simple case of 4 digits', function () {
+        expect(divisionStringified(10000, 10)).toBe('1,000');
+    });
+
+    it('should handle a case of 6 digits', function () {
+        expect(divisionStringified(100000, 1)).toBe('100,000');
+    });
+
+    it('should handle a case of > 6 digits', function () {
+        expect(divisionStringified(1000000, 1)).toBe('1,000,000');
+    });
+
+    it('should round decimals', function () {
+        expect(divisionStringified(1, 10)).toBe('0');
+        expect(divisionStringified(123456789, 10000)).toBe('12,346');
+    });
 });
