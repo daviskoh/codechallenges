c37272929c0581b077f88a83d106cba2cb58de16
daviskoh <koh.davis.0@gmail.com>
2014-12-03 15:04:56 -0500

    initial commit for dash insert

---
+++ javascript/dash_insert/lib/dash_insert.js
@@ -0,0 +1,31 @@
+// insert dashes bet 2 odd number digits
+// 0 is not odd num
+
+function isOdd(numString) {
+    if (numString === '0') return;
+
+    return parseInt(numString) % 3 === 0;
+}
+
+function dashInsert (n) {
+    var nString = n.toString(),
+        result = '';
+
+    // iterate through nString
+        // if nString[i] && nString[i + 1] are odd nums
+            // then result += nString[i] + '-'
+        // else
+            // then result += nString[i]
+
+    for (var i = 0; i < nString.length; i++) {
+        if (isOdd(nString[i]) && isOdd(nString[i + 1])) {
+            result += nString[i] + '-';
+        } else {
+            result += nString[i];
+        }
+    }
+
+    return result;
+}
+
+module.exports = dashInsert;
---
+++ javascript/dash_insert/spec/dash_insert.spec.js
@@ -0,0 +1,24 @@
+var dashInsert = require('../lib/dash_insert.js');
+
+describe('dashInsert', function() {
+    it('is a function', function () {
+        expect(typeof dashInsert).toBe('function');
+    });
+
+    it('should not insert a dash for a number with no odd num digits', function () {
+        expect(dashInsert(842)).toBe('842');
+    });
+
+    it('should handle a simple 2 digit case', function () {
+        expect(dashInsert(99)).toBe('9-9');
+    });
+
+    it('should not treat 0 as a odd num', function () {
+        expect(dashInsert(8008)).toBe('8008');
+    });
+
+    it('should handle single digits', function () {
+        expect(dashInsert(8)).toBe('8');
+        expect(dashInsert(3)).toBe('3');
+    });
+});
