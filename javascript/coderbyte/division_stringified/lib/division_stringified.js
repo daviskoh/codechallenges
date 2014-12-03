4e2d2ecff8e24d69836651c06e28cbc8ddb4c515
daviskoh <koh.davis.0@gmail.com>
2014-12-02 21:48:34 -0500

    handle < 4 digits

--- javascript/coderbyte/division_stringified/lib/division_stringified.js
+++ javascript/coderbyte/division_stringified/lib/division_stringified.js
@@ -1,5 +1,9 @@
-function divisionStringified (argument) {
-    
+// divide n1 by n2
+// return result as string w/ properly formatted commas
+// if result is 3 digits long, return num w/ no commas
+
+function divisionStringified (n1, n2) {
+    return (n1 / n2).toString();
 }
 
 module.exports = divisionStringified;
--- javascript/coderbyte/division_stringified/spec/division_stringified.spec.js
+++ javascript/coderbyte/division_stringified/spec/division_stringified.spec.js
@@ -4,4 +4,8 @@ describe('divisionStringified', function() {
     it('is a function', function () {
         expect(typeof divisionStringified).toBe('function');
     });
+
+    it('should handle a number less than 4 digits', function () {
+        expect(divisionStringified(3, 1)).toBe('3');
+    });
 });
