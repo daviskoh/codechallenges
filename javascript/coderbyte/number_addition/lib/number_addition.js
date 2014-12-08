6068d369be10023665693c73a48200c157f40329
daviskoh <koh.davis.0@gmail.com>
2014-12-08 13:49:27 -0500

    all specs passing

--- javascript/coderbyte/number_addition/lib/number_addition.js
+++ javascript/coderbyte/number_addition/lib/number_addition.js
@@ -1,5 +1,15 @@
-function numberAddition (argument) {
-    
+function numberAddition (string) {
+    var numbersArray = string.match(/[0-9]+/g);
+
+    if (numbersArray) {
+        return numbersArray.map(function (element) {
+            return parseInt(element);
+        }).reduce(function (a, b) {
+            return a + b;
+        });
+    }
+
+    return 0;
 }
 
 module.exports = numberAddition;
--- javascript/coderbyte/number_addition/spec/number_addition.spec.js
+++ javascript/coderbyte/number_addition/spec/number_addition.spec.js
@@ -4,4 +4,24 @@ describe('numberAddition', function() {
     it('is a function', function () {
         expect(typeof numberAddition).toBe('function');
     });
+
+    it('should handle an empty string', function () {
+        expect(numberAddition('')).toBe(0);
+    });
+
+    it('should handle a string without any numbers', function () {
+        expect(numberAddition('asdf')).toBe(0);
+    });
+
+    it('should handle a simple string w/ 1 num', function () {
+        expect(numberAddition('1')).toBe(1);
+    });
+
+    it('should handle mulit char strings', function () {
+        expect(numberAddition('1bd4dfa4')).toBe(9);
+    });
+
+    it('should handle mulit digiti nums', function () {
+        expect(numberAddition('123absdf888')).toBe(123 + 888);
+    });
 });
