68f865c18e45458d286324e18f175dc1dc532c5c
daviskoh <koh.davis.0@gmail.com>
2014-12-08 13:57:30 -0500

    recursive approach

--- javascript/coderbyte/number_addition/lib/number_addition.js
+++ javascript/coderbyte/number_addition/lib/number_addition.js
@@ -1,15 +1,23 @@
 function numberAddition (string) {
-    var numbersArray = string.match(/[0-9]+/g);
+    // var numbersArray = string.match(/[0-9]+/g);
 
-    if (numbersArray) {
-        return numbersArray.map(function (element) {
-            return parseInt(element);
-        }).reduce(function (a, b) {
-            return a + b;
-        });
-    }
+    // if (numbersArray) {
+    //     return numbersArray.map(function (element) {
+    //         return parseInt(element);
+    //     }).reduce(function (a, b) {
+    //         return a + b;
+    //     });
+    // }
 
-    return 0;
+    // return 0;
+
+    if (!string.length) return 0;
+
+    var numMatch = string.match(/[0-9]+/);
+
+    if (!numMatch) return 0;
+
+    return parseInt(numMatch[0]) + numberAddition(string.substring(numMatch.index + numMatch[0].length));
 }
 
 module.exports = numberAddition;
