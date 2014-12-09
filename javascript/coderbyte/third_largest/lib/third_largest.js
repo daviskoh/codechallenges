3f0683ba5d461f86b497ce856321f0f03a667267
daviskoh <koh.davis.0@gmail.com>
2014-12-08 19:57:58 -0500

    handles all cases

--- javascript/coderbyte/third_largest/lib/third_largest.js
+++ javascript/coderbyte/third_largest/lib/third_largest.js
@@ -1,5 +1,18 @@
-function thirdLargest(argument) {
-    
+// return 3rd largest string in array
+
+function thirdLargest(array) {
+    // sort array by string length
+    var sorted = array.sort(function (a, b) {
+        return b.length - a.length;
+    });
+
+    // find length of string at index 2
+    var length = sorted[2].length;
+
+    for (var i = array.length - 1; i >= 0; i--) {
+        var currentElement = array[i];
+        if (currentElement.length === length) return currentElement;
+    }
 }
 
 module.exports = thirdLargest;
--- javascript/coderbyte/third_largest/spec/third_largest.spec.js
+++ javascript/coderbyte/third_largest/spec/third_largest.spec.js
@@ -4,4 +4,16 @@ describe('thirdLargest', function() {
     it('is a function', function () {
         expect(typeof thirdLargest).toBe('function');
     });
+
+    it('should handle a simple case', function () {
+        expect(thirdLargest(['ccc', 'a', 'bb'])).toBe('a');
+    });
+
+    it('given a tie, should select the last occurence', function () {
+        expect(thirdLargest(['eeee', 'aa', 'bb', 'meoooow'])).toBe('bb');
+    });
+
+    it('should handle multi occurences', function () {
+        expect(thirdLargest(['eieie', 'ccccc', 'alsdkfjalksdjf', 'zzzzz'])).toBe('zzzzz');
+    });
 });
