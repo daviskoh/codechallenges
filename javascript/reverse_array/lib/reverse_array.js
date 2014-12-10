58059bba29d8d14ef069dbcbf36fec65f49de02c
daviskoh <koh.davis.0@gmail.com>
2014-12-10 13:30:10 -0500

    iterative solution

--- javascript/reverse_array/lib/reverse_array.js
+++ javascript/reverse_array/lib/reverse_array.js
@@ -1,9 +1,16 @@
 function reverseArray(array) {
-    if (!array.length) return array;
+    // if (!array.length) return array;
 
-    var endIndex = array.length - 1;
+    // var endIndex = array.length - 1;
 
-    return [array[endIndex]].concat(reverseArray(array.slice(0, endIndex)));
+    // return [array[endIndex]].concat(reverseArray(array.slice(0, endIndex)));
+
+    var result = [];
+    for (var i = array.length - 1; i >= 0; i--) {
+        result.push(array[i]);
+    }
+
+    return result;
 }
 
 module.exports = reverseArray;
