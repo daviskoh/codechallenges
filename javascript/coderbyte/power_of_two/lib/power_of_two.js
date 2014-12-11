be6367c93b332b02194ba4d75199c5cf7a2b3a01
daviskoh <koh.davis.0@gmail.com>
2014-12-11 11:47:09 -0500

    all cases

--- javascript/coderbyte/power_of_two/lib/power_of_two.js
+++ javascript/coderbyte/power_of_two/lib/power_of_two.js
@@ -1,5 +1,40 @@
-function powerOfTwo(argument) {
-    
+// return true if power of 2
+// else return false
+
+// power of 2 if
+    // i / 2 is divisible by 2...
+
+// ex. 8
+// 8 / 2 = 4
+// 4 / 2 = 2
+// 2 / 2 = 1
+
+// until result < 3
+    // keep dividing by 2
+    // if result is 2
+        // return true
+    // else
+        // return false
+
+
+
+function powerOfTwo(i) {
+    if (i < 0) return 'false';
+
+    var result = i,
+        returnVal;
+
+    while (result > 3) {
+        result = result / 2;
+
+        if (result === 2) {
+            returnVal = 'true';
+        } else {
+            returnVal = 'false';
+        }
+    }
+
+    return returnVal;
 }
 
 module.exports = powerOfTwo;
--- javascript/coderbyte/power_of_two/spec/power_of_two.spec.js
+++ javascript/coderbyte/power_of_two/spec/power_of_two.spec.js
@@ -4,4 +4,22 @@ describe('powerOfTwo', function() {
     it('is a function', function () {
         expect(typeof powerOfTwo).toBe('function');
     });
+
+    it('should return false for negative nums', function () {
+        expect(powerOfTwo(-1)).toBe('false');
+    });
+
+    it('should handle a simple n', function () {
+        expect(powerOfTwo(4)).toBe('true');
+    });
+
+    it('should handle complex case', function () {
+        expect(powerOfTwo(8)).toBe('true');
+        expect(powerOfTwo(16)).toBe('true');
+    });
+
+    it('should reject false nums', function () {
+        expect(powerOfTwo(123)).toBe('false');
+        expect(powerOfTwo(7)).toBe('false');
+    });
 });
