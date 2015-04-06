865a2238cdf5a4bb083bb169bec9f66e8ee137ca
Davis Koh <koh.davis.0@gmail.com>
2015-04-06 16:57:42 -0400

    add fizzbuzz

---
+++ swift/FizzBuzz/FizzBuzz.swift
@@ -0,0 +1,15 @@
+func fizzBuzzString(n: Int) -> String {
+    if n % 15 == 0 {
+        return "FizzBuzz"
+    } else if n % 5 == 0 {
+        return "Buzz"
+    } else if n % 3 == 0 {
+        return "Fizz"
+    }
+
+    return String(n)
+}
+
+for i in 1...100 {
+    println(fizzBuzzString(i))
+}
