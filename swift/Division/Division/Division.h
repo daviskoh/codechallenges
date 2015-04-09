e7ec12d358ff63989ee22f766ef9e434d4dbb249
daviskoh <koh.davis.0@gmail.com>
2015-04-09 18:14:19 -0400

    init

---
+++ swift/Division/Division/Division.h
@@ -0,0 +1,19 @@
+//
+//  Division.h
+//  Division
+//
+//  Created by Davis Koh on 4/9/15.
+//  Copyright (c) 2015 com.DavisKoh. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Division.
+FOUNDATION_EXPORT double DivisionVersionNumber;
+
+//! Project version string for Division.
+FOUNDATION_EXPORT const unsigned char DivisionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Division/PublicHeader.h>
+
+
