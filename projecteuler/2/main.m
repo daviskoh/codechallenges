860579e0a38b46cfd621d526c1241578dfd9995c
Davis Koh <koh.davis.0@gmail.com>
2015-05-11 18:28:36 -0400

    skeleton

---
+++ projecteuler/2/main.m
@@ -0,0 +1,11 @@
+// declare fibonacci helper block
+BOOL (^isFibonacci)(int) = ^(int n) {
+
+}
+
+int main() {
+    @autoreleasepool {
+        // iterate over range of int from 1 to 4m
+        // if even, then add up
+    }
+}
