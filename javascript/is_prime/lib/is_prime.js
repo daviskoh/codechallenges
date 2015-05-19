03d6a4daf02199894531cd2ac5909d6605ea6672
Davis Koh <koh.davis.0@gmail.com>
2015-05-19 10:48:48 -0400

    do min to handle nums greater than 1

--- javascript/is_prime/lib/is_prime.js
+++ javascript/is_prime/lib/is_prime.js
@@ -2,6 +2,8 @@ function isPrime(n) {
     if (n < 2) {
         return false;
     }
+
+    return true;
 }
 
 module.exports = isPrime;
--- javascript/is_prime/spec/is_prime.spec.js
+++ javascript/is_prime/spec/is_prime.spec.js
@@ -12,4 +12,11 @@ describe('isPrime', function() {
         expect(isPrime(0)).toBe(false);
         expect(isPrime(-123)).toBe(false);
     });
+
+    it('should handle single digit numbers', function () {
+        expect(isPrime(2)).toBe(true);
+        expect(isPrime(3)).toBe(true);
+        expect(isPrime(5)).toBe(true);
+        expect(isPrime(7)).toBe(true);
+    });
 });
