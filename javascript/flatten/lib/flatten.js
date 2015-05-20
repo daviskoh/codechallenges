aac7e2ea5d71e0b1e090285840617dd57c16a844
Davis Koh <koh.davis.0@gmail.com>
2015-05-20 11:10:02 -0400

    copy over from /test directory

--- javascript/flatten/lib/flatten.js
+++ javascript/flatten/lib/flatten.js
@@ -1,5 +1,13 @@
-function flatten(argument) {
-    
+function flatten(array) {
+    var result = [];
+
+    for (var i = 0, len = array.length; i < len; i++) {
+        var element = array[i];
+        if (typeof element === 'number') result.push(element);
+        if (typeof element === 'object') result = result.concat(flatten(element));
+    }
+
+    return result;
 }
 
 module.exports = flatten;
--- javascript/flatten/spec/flatten.spec.js
+++ javascript/flatten/spec/flatten.spec.js
@@ -4,4 +4,21 @@ describe('flatten', function() {
     it('is a function', function () {
         expect(typeof flatten).toBe('function');
     });
+
+    it('should handle an empty array', function () {
+        expect(flatten([])).toEqual([]);
+    });
+
+    it('should handle an array w/ element', function () {
+        expect(flatten([1,2,3,4,5])).toEqual([1,2,3,4,5]);
+    });
+
+    it('should handle an array 2 levels deep', function () {
+        expect(flatten([1,2,3,[4,5],6])).toEqual([1,2,3,4,5,6]);
+    });
+
+    it('should handle an array multi levels deep', function () {
+        expect(flatten([1,2,[3,4,[5,6]]])).toEqual([1,2,3,4,5,6]);
+    });
 });
+
