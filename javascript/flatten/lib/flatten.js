a2cd08ce11b179153666d9a8cd24a161df6ecc27
Davis Koh <koh.davis.0@gmail.com>
2015-05-20 11:03:58 -0400

    init

---
+++ javascript/flatten/lib/flatten.js
@@ -0,0 +1,5 @@
+function flatten(argument) {
+    
+}
+
+module.exports = flatten;
---
+++ javascript/flatten/spec/flatten.spec.js
@@ -0,0 +1,7 @@
+var flatten = require('../lib/flatten.js');
+
+describe('flatten', function() {
+    it('is a function', function () {
+        expect(typeof flatten).toBe('function');
+    });
+});
