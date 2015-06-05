20d885a007210a03f968b79720ea29e68d6f80f8
Davis Koh <koh.davis.0@gmail.com>
2015-06-05 17:27:39 -0400

    recursive solution

--- javascript/swap_case/swap_case.js
+++ javascript/swap_case/swap_case.js
@@ -1,17 +1,29 @@
 function swapCase(string) {
-    var result = '';
+    // iterative solution
 
-    for (var i = 0, len = string.length; i < len; i++) {
-        var char = string[i];
+    // var result = '';
+
+    // for (var i = 0, len = string.length; i < len; i++) {
+        // var char = string[i];
         // check if already capitalized
-        if (char === char.toUpperCase()) {
-            result += char.toLowerCase();
-        } else {
-            result += char.toUpperCase();
-        }
-    }
+        // if (char === char.toUpperCase()) {
+            // result += char.toLowerCase();
+        // } else {
+            // result += char.toUpperCase();
+        // }
+    // }
 
-    return result;
+    // return result;
+
+    // recursive solution
+    if (!string.length) return string;
+
+    // return the 1st char swap-cased then substring via subroutine
+    var char = string[0],
+        upperCaseChar = char.toUpperCase(),
+        swappedChar = char === upperCaseChar ? char.toLowerCase() : upperCaseChar;
+
+    return swappedChar + swapCase(string.substring(1));
 }
 
 module.exports = swapCase;
