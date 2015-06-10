6194db24fdd563c083b265f7bc6b075f0b66e6a9
Davis Koh <koh.davis.0@gmail.com>
2015-06-10 13:54:27 -0400

    handles empty strings

--- javascript/string_frequency/iterative/string_frequency.js
+++ javascript/string_frequency/iterative/string_frequency.js
@@ -1,5 +1,6 @@
-function iterative(argument) {
-    
+// write a func that counts the occurrences of a substring in a given string
+function iterative(string, substring) {
+    return 0;
 }
 
 module.exports = iterative;
--- javascript/string_frequency/iterative/string_frequency.spec.js
+++ javascript/string_frequency/iterative/string_frequency.spec.js
@@ -4,4 +4,9 @@ describe('stringFrequency', function() {
     it('is a function', function () {
         expect(typeof stringFrequency).toBe('function');
     });
+
+    it('should return 0 for empty strings', function () {
+        expect(stringFrequency('', 'asdf')).toBe(0);
+        expect(stringFrequency('', '')).toBe(0);
+    });
 });
