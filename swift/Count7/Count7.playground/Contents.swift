132a7c8fb60009119c87d5ccc7f5e8a8a20d2515
daviskoh <koh.davis.0@gmail.com>
2016-05-04 12:06:00 -0400

    add iterative solution

--- swift/Count7/Count7.playground/Contents.swift
+++ swift/Count7/Count7.playground/Contents.swift
@@ -8,21 +8,34 @@ import UIKit
 // if 7 return 1 + count7(substring w/o 1st char)
 // else return count7(substring w/o 1st char)
 
-func count7(n: Int?) -> Int {
-  if n == nil {
-    return 0
+//func count7(n: Int?) -> Int {
+//  if n == nil {
+//    return 0
+//  }
+//
+//  let s = String(n!)
+//  
+//  // NOTE: starting in Swift 2, type is used INSTEAD of Int
+//  let substringAsInt = Int(s[s.startIndex.advancedBy(1)..<s.endIndex])
+//
+//  if s[s.startIndex] == "7" {
+//    return 1 + count7(substringAsInt)
+//  } else {
+//    return count7(substringAsInt)
+//  }
+//}
+
+func count7(n: Int) -> Int {
+  let s = String(n)
+  var count = 0
+
+  for char in s.characters {
+    if char == "7" {
+      count++
+    }
   }
 
-  let s = String(n!)
-  
-  // NOTE: starting in Swift 2, type is used INSTEAD of Int
-  let substringAsInt = Int(s[s.startIndex.advancedBy(1)..<s.endIndex])
-
-  if s[s.startIndex] == "7" {
-    return 1 + count7(substringAsInt)
-  } else {
-    return count7(substringAsInt)
-  }
+  return count
 }
 
 print(count7(17)) //=> 1
