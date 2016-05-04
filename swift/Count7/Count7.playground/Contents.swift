a9fd1dceed1bfd707af0eca23210057c46f6f921
daviskoh <koh.davis.0@gmail.com>
2016-05-04 12:02:55 -0400

    count7 swift challenge recursive

---
+++ swift/Count7/Count7.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+// count 7
+// take n: Int
+// String(n)
+// if char length == 0 then return 0
+// check 1st char of string
+// if 7 return 1 + count7(substring w/o 1st char)
+// else return count7(substring w/o 1st char)
+
+func count7(n: Int?) -> Int {
+  if n == nil {
+    return 0
+  }
+
+  let s = String(n!)
+  
+  // NOTE: starting in Swift 2, type is used INSTEAD of Int
+  let substringAsInt = Int(s[s.startIndex.advancedBy(1)..<s.endIndex])
+
+  if s[s.startIndex] == "7" {
+    return 1 + count7(substringAsInt)
+  } else {
+    return count7(substringAsInt)
+  }
+}
+
+print(count7(17)) //=> 1
+print(count7(123771171)) //=> 3
---
+++ swift/Count7/readme.md
@@ -0,0 +1,2 @@
+Given an Int, count the number of occurrences of 7 as a digit
+
