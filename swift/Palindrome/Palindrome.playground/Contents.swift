f34b88c7ab0260325c3a7db7948e87677b413499
daviskoh <koh.davis.0@gmail.com>
2016-05-06 12:42:42 -0400

    add primitive custom reverse func for string

--- swift/Palindrome/Palindrome.playground/Contents.swift
+++ swift/Palindrome/Palindrome.playground/Contents.swift
@@ -4,15 +4,28 @@
 // if a palindrome/not
 // 2) ignore whitespace
 // ex - "taco cat"
+// 3) not allowed to use .reverse()
 
 import UIKit
+
+// abc
+// c ab
+func reverseString(s: String) -> String {
+  if s.characters.count == 1 {
+    return s
+  }
+
+  return String(s.characters.last!) + reverseString((s.substringToIndex(s.endIndex.advancedBy(-1))))
+}
+
+print(reverseString("abc"))
 
 func stripWhitespace(s: String) -> String {
   return s.stringByReplacingOccurrencesOfString(" ", withString: "")
 }
 
 func palindrome(s: String) -> Bool {
-  return stripWhitespace(s) == stripWhitespace(String(s.characters.reverse()))
+  return stripWhitespace(s) == stripWhitespace(reverseString(s))
 }
 
 print(palindrome("racecar"))
