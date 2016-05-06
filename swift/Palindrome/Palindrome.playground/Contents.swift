5b331c6873e9986cc2caea74867f882c23625bc3
daviskoh <koh.davis.0@gmail.com>
2016-05-06 12:25:07 -0400

    ignore whitespace

--- swift/Palindrome/Palindrome.playground/Contents.swift
+++ swift/Palindrome/Palindrome.playground/Contents.swift
@@ -1,14 +1,21 @@
 // Palindrome - word that is the same when reversed
 // ex - racecar
-// write a func that takes a string and returns true/false 
+// 1) write a func that takes a string and returns true/false
 // if a palindrome/not
+// 2) ignore whitespace
+// ex - "taco cat"
 
 import UIKit
 
+func stripWhitespace(s: String) -> String {
+  return s.stringByReplacingOccurrencesOfString(" ", withString: "")
+}
+
 func palindrome(s: String) -> Bool {
-  return s == String(s.characters.reverse())
+  return stripWhitespace(s) == stripWhitespace(String(s.characters.reverse()))
 }
 
 print(palindrome("racecar"))
 print(palindrome("raceca"))
 
+print(palindrome("taco cat"))
