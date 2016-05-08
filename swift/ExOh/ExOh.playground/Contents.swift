5ad20ca3fdec0a48ab69bfed8ef843a8eb09abb7
daviskoh <koh.davis.0@gmail.com>
2016-05-08 16:00:08 -0400

    add "more efficient" solution i think

--- swift/ExOh/ExOh.playground/Contents.swift
+++ swift/ExOh/ExOh.playground/Contents.swift
@@ -9,8 +9,25 @@ func hasEqualXAndO(s: String) -> Bool {
   // count num of o's
   // return x == o
 
-  let xCount = s.componentsSeparatedByString("x").count - 1
-  let oCount = s.componentsSeparatedByString("o").count - 1
+//  let xCount = s.componentsSeparatedByString("x").count - 1
+//  let oCount = s.componentsSeparatedByString("o").count - 1
+
+  // IF componentsSeparatedByString already iterates
+  // through s in order to count components...
+  // it may be more efficient to simply count myself using
+  // a single iteration vs the presumably 2 from previous solution
+  var xCount = 0
+  var oCount = 0
+
+  for char in s.characters {
+    if char == Character("x") {
+      xCount++
+    }
+
+    if char == Character("o") {
+      oCount++
+    }
+  }
 
   if xCount == 0 || oCount == 0 {
     return false
